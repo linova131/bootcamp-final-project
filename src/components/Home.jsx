8c3a325ab68971f28c8185746be36bc54cb7ffbb
tpo-ssraj <ssraj@travelers.com>
2021-05-03 12:59:45 -0400

    useEffect

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,14 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import axios from 'axios';
 import apiKey from '../config';
 
 function Home(){
 
+    const [movieData, setMovieData] = useState([]);
+
     function fetchMovies(){
         axios.get(`https://api.themoviedb.org/3/movie/top_rated?api_key=${apiKey}&language=en-US&page=1`)
-        .then(response => console.log(response))
+        .then(response => setMovieData(response.data.results))
     }
 
     useEffect(() => {
