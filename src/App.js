0bb5e978a30786dedcb9dc860d076a577a5c062d
tpo-hmarchma <77006539+tpo-hmarchma@users.noreply.github.com>
2021-05-03 15:41:33 -0500

    header component

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import './App.css';
 import Home from './components/Home';
 import { Route, BrowserRouter, Switch } from 'react-router-dom';
 import MovieDetail from './components/MovieDetail';
+import Header from './components/Header';
 
 function App() {
   return (
     <BrowserRouter>
+    <Header />
     <div>
     <Switch>
       <Route exact path='/' component={Home}></Route>
