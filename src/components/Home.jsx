752faf968b737b22e60249467ee28050a5d59d78
tpo-mtowles <77006920+tpo-mtowles@users.noreply.github.com>
2021-05-04 14:12:56 -0400

    formatting on home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,34 +1,34 @@
-import React, {useEffect, useState, useContext} from 'react';
-import Context from '../Context';
-import MovieContainer from './MovieContainer';
+import React, { useEffect, useState, useContext } from "react";
+import Context from "../Context";
+import MovieContainer from "./MovieContainer";
 
-function Home(){
-    const {movieData, fetchMovies} = useContext(Context);
+function Home() {
+  const { movieData, fetchMovies } = useContext(Context);
 
-    useEffect(() => {
-        fetchMovies();
-    }, []);
+  useEffect(() => {
+    fetchMovies();
+  }, []);
 
-    return (
-        <main className="main-content">
-        <div className="container">
-          <div className="page">
-            
-
-              <div className="row">
-                <div className="col-md-4">
-                 {
-                   (movieData.length > 0)
-                   ? <MovieContainer data={movieData} />
-                   : <p>Loading...</p>
-                 } 
-                </div>
-    
+  return (
+    <main className="main-content">
+      <div className="container">
+        <div className="page">
+          <div className="row ">
+            <div
+              className="col-md-12
+            "
+            >
+              {movieData.length > 0 ? (
+                <MovieContainer data={movieData} />
+              ) : (
+                <p>Loading...</p>
+              )}
             </div>
           </div>
         </div>
-      </main>
-    )
+      </div>
+    </main>
+  );
 }
 
 export default Home;
