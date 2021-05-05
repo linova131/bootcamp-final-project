5a11bd214eda10a4e02aa7c40e04a3c9cb098eb0
tpo-ssraj <77004985+tpo-ssraj@users.noreply.github.com>
2021-05-05 13:52:18 -0400

    premiere boxes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState, useContext } from "react";
 import Context from "../Context";
 import MovieContainer from "./MovieContainer";
 
+
 function Home() {
   const { movieData, fetchMovies, searchResults } = useContext(Context);
 
@@ -9,15 +10,13 @@ function Home() {
     fetchMovies();
   }, []);
 
+
   return (
     <main className="main-content">
       <div className="container">
         <div className="page">
           <div className="row ">
-            <div
-              className="col-md-12
-            "
-            >
+            <div className="col-md-12">
               {movieData.length > 0 ? (
                 <MovieContainer data={movieData} />
               ) : (
@@ -26,7 +25,28 @@ function Home() {
             </div>
           </div>
         </div>
-      </div>
+        <p></p>
+      <div className="row">
+							<div className="col-md-4">
+								<h2 className="section-title">May premieres</h2>
+								<ul className="movie-schedule">
+									{/* <ComingSoon data={comingSoonMovies}></ComingSoon> */}
+								</ul>
+							</div>
+							<div className="col-md-4">
+								<h2 className="section-title">June premieres</h2>
+								<ul className="movie-schedule">
+					
+								</ul> 
+							</div>
+							<div class="col-md-4">
+								<h2 class="section-title">July premieres</h2>
+								<ul class="movie-schedule">
+								
+								</ul> 
+							</div>
+						</div> 
+            </div>
     </main>
   );
 }
